501dc35d197c86ee43400981ffeeac6181e160b6
Yufang  Zhang <yufang.zhang14@imperial.ac.uk>
2015-07-29 20:34:35 +0100

    recommit chef-repo

---
+++ chef-repo/metadata.rb
@@ -0,0 +1,7 @@
+name             'chef-repo'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures chef-repo'
+long_description 'Installs/Configures chef-repo'
+version          '0.1.0'
