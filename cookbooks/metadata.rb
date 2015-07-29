ead415f5b21854e8d5228ea042c4782e18ff7d4a
Yufang  Zhang <yufang.zhang14@imperial.ac.uk>
2015-07-29 20:21:20 +0100

    pre-requisites done

---
+++ cookbooks/metadata.rb
@@ -0,0 +1,7 @@
+name             'cookbooks'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures cookbooks'
+long_description 'Installs/Configures cookbooks'
+version          '0.1.0'
