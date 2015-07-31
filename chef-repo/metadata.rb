ab039417cc95a95e70896d3e375b10b717617b74
Yufang  Zhang <yufang.zhang14@imperial.ac.uk>
2015-07-31 14:50:21 +0100

    delete chef folder

--- chef-repo/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'chef-repo'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
-license          'All rights reserved'
-description      'Installs/Configures chef-repo'
-long_description 'Installs/Configures chef-repo'
-version          '0.1.0'
--- chef-repo/setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Provisioning virtual machine..."
-
